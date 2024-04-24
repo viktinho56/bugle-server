0cc3207d1f311c6180abcad1bea4c633cd75f953
Victor Fadipe <tinsofttechnologiesltd@gmail.com>
2024-04-24 12:55:50 +0100

    third commit

--- startup/config.js
+++ startup/config.js
@@ -1,6 +1,6 @@
 const config = require("config");
 module.exports = function () {
-  if (!process.env.jwtPrivateKey) {
+  if (!config.get("jwtPrivateKey")) {
     throw new Error("FATAL ERROR: jwtPrivateKey is not defined.");
   }
 };
